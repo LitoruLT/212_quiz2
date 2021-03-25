1e5482000a589c83fcfbf9812ee025a96d2ba4bd
Litoru <puwarin2001@gmail.com>
2021-03-25 18:05:55 +0700

    2-4

--- Quiz2_2.c
+++ Quiz2_2.c
@@ -21,7 +21,7 @@ void main()
     {
         for(rows=0 ; rows<Max_ROWS ; rows++)
         {
-            for(i=0 ; i<M ; i++)
+            for(i=0 ; i<N ; i++)
             {
                 for(j=0 ; j<2 ; j++)
                 {
@@ -39,7 +39,11 @@ void main()
             printf("o");
 
         }
-        printf("\n");
+        if(cols+1<Max_COLS)
+        {
+            printf("\n");
+        }
+
     }
 
 }
