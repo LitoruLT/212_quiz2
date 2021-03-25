fb51d43ce780bab02812c3f4d7b8b6984b0490e2
Litoru <puwarin2001@gmail.com>
2021-03-25 17:07:53 +0700

    1-1

---
+++ Quiz2_1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int checkCUBE(int n);
+
+void main()
+{
+    long long int n;
+    scanf("%lli",&n);
+    if(checkCUBE(n))
+    {
+
+    }
+
+
+}
+
+int checkCUBE(int n)
+{
+    int check=0,i;
+
+    for(i=0 ; i<3 ; i++)
+    {
+
+    }
+
+    return check;
+}
