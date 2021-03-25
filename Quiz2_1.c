28c45739814d3f7a44dbc082b8560f576da2ba0d
Litoru <puwarin2001@gmail.com>
2021-03-25 17:32:39 +0700

    1-5

--- Quiz2_1.c
+++ Quiz2_1.c
@@ -30,17 +30,14 @@ void main()
 long long int check_CUBE_NUM(long long int n)
 {
     long long int i,j=n;
-    j=n-j+1;
     while(1)
     {
-        printf("j == %lli\n",j);
         if(j*j*j == n)
         {
             return 1;
         }
         else if(j*j*j<n)
         {
-
             return 0;
         }
         j=j-1;
@@ -52,10 +49,8 @@ void check_POS_CUBE_FREE(long long int n)
     long long int i,j=3,pos=2;
     while(j<=n)
     {
-        if(!check_CUBE_NUM(n))
-        {
+        if(!check_CUBE_NUM(j))
             pos++;
-        }
         j++;
     }
     printf("%lli",pos);
