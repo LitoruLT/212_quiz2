cda68ad5cbb2ebd67f2c8368774df13375d3d83b
Litoru <puwarin2001@gmail.com>
2021-03-25 17:47:16 +0700

    2-1

--- Quiz2_2.c
+++ Quiz2_2.c
@@ -2,4 +2,28 @@
 
 void main()
 {
+    long long int M,N,i,j;
+    scanf("%lli",&M);
+    scanf("%lli",&N);
+
+    long long int data[N][M];
+
+    for(i=0 ; i<N ; i++)
+    {
+        for(j=0 ; j<M ; j++)
+        {
+           scanf("%lli",data[i][j]);
+        }
+    }
+
+    long long int rows=0,Max_ROWS=70;
+    while(rows < Max_ROWS)
+    {
+        printf("o");
+
+
+        rows++;
+    }
+
 }
+
