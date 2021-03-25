b19f1ecabc08549a1f4eb56d234ff1a94d02e9af
Litoru <puwarin2001@gmail.com>
2021-03-25 18:20:33 +0700

    3-1

--- Quiz2_3.c
+++ Quiz2_3.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 void main()
 {
+
+    char data1[8],data2[8];
+
+    long long int n,i,j;
+    scanf("%s",data1);
+
+    for(i=strlen(data1),j=0 ; i>=0 ;i=i-1,j++)
+    {
+        data2[0]=data1[i];
+        j++;
+    }
+
+    printf("%s    %s",data1,data2);
 
 }
