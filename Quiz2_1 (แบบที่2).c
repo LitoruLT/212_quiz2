028062ae111242ed86cb1cfe1ae0527bef191d06
Litoru <puwarin2001@gmail.com>
2021-03-25 19:46:29 +0700

    2_1แยกแบบไว้ส่ง (ส่งแบบ 2_1 ธรรมดาไป)

--- Quiz2_1 (แบบที่2).c
+++ Quiz2_1 (แบบที่2).c
@@ -47,12 +47,33 @@ long long int check_CUBE_NUM(long long int n)
 void check_POS_CUBE_FREE(long long int n)
 {
 
-    long long int i=0,j=3,pos=2;
+    long long int i=0,j=3,pos=2,saveCUBE[n],k;
+    for(k=0; k<n ; k++)
+        saveCUBE[k]=0;
     while(j<=n)
     {
         if(!check_CUBE_NUM(j))
         {
+            if(saveCUBE[0]!=0)
+            {
+                for(k=0; saveCUBE[k]!=0 ; k++)
+                {
+                    if( !(j%saveCUBE[k]==0) && saveCUBE[k]!=0)
+                    {
+                        pos++;
+                    }
+                }
+            }
+            else
+            {
                 pos++;
+            }
+
+        }
+        else
+        {
+            saveCUBE[i]=j;
+            i++;
         }
         j++;
     }
